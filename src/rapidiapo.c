46724883079c973661965a37bc1c50a57e460942
Sylvain Beucler <beuc@beuc.net>
2007-10-07 11:01:52 +0200

    New transition effects, build system fixes

--- src/rapidiapo.c
+++ src/rapidiapo.c
@@ -27,10 +27,10 @@
 #if defined _WIN32 || defined __WIN32__ || defined __CYGWIN__
 # define WIN32_LEAN_AND_MEAN
 # include <windows.h>
-
-#define strcasestr strstr
 #endif
 
+#define IMIN(a,b) ((a < b) ? a : b)
+#define IMAX(a,b) ((a > b) ? a : b)
 
 #include "SDL.h"
 #include "SDL_image.h"
@@ -77,8 +77,8 @@ static inline int myrand(int limit)
 
 static SDL_Surface *screen;
 static SDL_Surface *previous_screen, *next_screen;
-static SDL_Surface *image1, *image2,
-  *resized1, *resized2;
+/* static SDL_Surface *image1, *image2, */
+/*   *resized1, *resized2; */
 static int last_update = 0;
 
 /* load_image() */
@@ -179,10 +179,10 @@ int main(int argc, char *argv[])
   FPSmanager framerate_manager;
   int quit = 0;
   int start;
-  SDL_Thread* background_loading;
+  SDL_Thread* background_loading = NULL;
 
   SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
-  screen = SDL_SetVideoMode(0, 0, 0, SDL_HWSURFACE | SDL_ANYFORMAT | SDL_FULLSCREEN);
+  screen = SDL_SetVideoMode(0, 0, 0, SDL_HWSURFACE | SDL_ANYFORMAT); // | SDL_FULLSCREEN
   if (screen == NULL)
     {
       error(_("Could not start the graphics display: %s\n"), SDL_GetError());
@@ -198,7 +198,14 @@ int main(int argc, char *argv[])
   next_screen = SDL_DisplayFormat(screen);
 
   SDL_initFramerate(&framerate_manager);
+  /* Nobody can notice more than 50 FPS - doing more is just wasting
+     CPU, which may be needed for other medias such as sound (think
+     timidity++...). This doesn't mean that all transition effects
+     have to use it, in particular dissolve cannot be rendered in
+     decent time, so we do need 100% CPU to get something close to
+     what we'd like. */
   SDL_setFramerate(&framerate_manager, 50);
+
   /*   { */
   /*     SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0)); */
   /*     SDL_Flip(screen); */
@@ -284,17 +291,37 @@ int main(int argc, char *argv[])
       SDL_BlitSurface(resized, NULL, next_screen, NULL);
       SDL_BlitSurface(screen, NULL, previous_screen, NULL);
 
-      last_update = start = SDL_GetTicks();
-      
-      if (SDL_MUSTLOCK(screen))
-	SDL_LockSurface(screen);
-      
+      /* Transition 1 - dissolve */
       unsigned int total = 0;
       unsigned int pos = 0;
+      /* Transition 2 - alpha blend */
+      /* Transition 3 - increamental zoom (OSX screensaver-style) */
+      SDL_Surface *zoombase = NULL;
+      {
+	/* For better quality, zoom from a bigger image than the
+	   screen, otherwise pixels will appear to "move" between
+	   zooms". Do not resize from the original image, which may be
+	   too big and slow down the zooming. */
+	SDL_Surface *resized = zoomSurface(image,
+					   2.0*screen->w / image->w,
+					   2.0*screen->w / image->w, SMOOTHING_ON);
+	/* Work around SDL 2.0.16 bug */
+	SDL_SetAlpha(resized, 0, 0);
+	/* Use the same RGBA order than the screen for fast
+	   blitting. zoomSurface keeps the same RGBA order when
+	   zooming 32bit surfaces. */
+	zoombase = SDL_DisplayFormat(resized);
+      }
 
+      last_update = start = SDL_GetTicks();
+
+
+      /**
+       * Transition
+       */
       int skip = 0;
-      while (!quit && !skip && ((SDL_GetTicks() - start) < 3000))
-      // while (!quit && !skip)
+      while (!quit && !skip) //  && ((SDL_GetTicks() - start) < 3000)
+	// while (!quit && !skip)
 	{
 	  SDL_Event event;
 	  while (SDL_PollEvent(&event))
@@ -314,143 +341,188 @@ int main(int argc, char *argv[])
 		}
 	    }
 
-	  /* Transition 1 */
-	  int randx, randy;
-	  int c1 = RAND_MAX / next_screen->w, c2 = RAND_MAX / next_screen->h;
-	  int pos_limit = (screen->h * screen->w - 1) / 4;
-	  /* int increment = (2000 + myrand(2000)) * next_screen->format->BytesPerPixel; */
-	  int increment = screen->pitch * screen->h / 4;
-	  for (int i = 0; i < 100; i++)
+	  int transition = 2;
+	  if (transition == 1)
 	    {
-	      Uint8 r=0, g=0, b=0;
-	      unsigned int array_index;
+	      /* Transition 1 - random dissolve */
+	      /* int randx, randy; */
+	      /* int c1 = RAND_MAX / next_screen->w, c2 = RAND_MAX / next_screen->h; */
+	      int pos_limit = (screen->h * screen->w - 1) / 4;
+	      /* int increment = (2000 + myrand(2000)) * next_screen->format->BytesPerPixel; */
+	      int increment = screen->pitch * screen->h / 4;
+	      for (int i = 0; i < 100; i++)
+		{
+		  /* Uint8 r=0, g=0, b=0; */
+		  unsigned int array_index;
 	      
-	      /* 1x1 pixel each time - too slow */
-	      /* randx = (int) round(1.0 * (next_screen->w-1) * rand() / RAND_MAX); */
-	      /* randy = (int) round(1.0 * (next_screen->h-1) * rand() / RAND_MAX); */
-	      /* get_pixel(next_screen, randx, randy, &r, &g, &b); */
-	      /* set_pixel(screen, randx, randy, r, g, b); */
+		  /* 1x1 pixel each time - too slow */
+		  /* randx = (int) round(1.0 * (next_screen->w-1) * rand() / RAND_MAX); */
+		  /* randy = (int) round(1.0 * (next_screen->h-1) * rand() / RAND_MAX); */
+		  /* get_pixel(next_screen, randx, randy, &r, &g, &b); */
+		  /* set_pixel(screen, randx, randy, r, g, b); */
 	      
-	      /* 	  randx = myrand(next_screen->w); */
-	      /* 	  randy = myrand(next_screen->h); */
+		  /* 	  randx = myrand(next_screen->w); */
+		  /* 	  randy = myrand(next_screen->h); */
 	      
-	      /* 	  while((randx = rand() / c1) >= next_screen->w); */
-	      /* 	  while((randy = rand() / c2) >= next_screen->h); */
+		  /* 	  while((randx = rand() / c1) >= next_screen->w); */
+		  /* 	  while((randy = rand() / c2) >= next_screen->h); */
 	      
-	      /* 	  pos = randy * next_screen->pitch + randx * next_screen->format->BytesPerPixel; */
+		  /* 	  pos = randy * next_screen->pitch + randx * next_screen->format->BytesPerPixel; */
 	      
-	      /* 	  pos = (unsigned)total * (screen->w / 10) % (screen->h * screen->w - 1) * next_screen->format->BytesPerPixel; */
-	      /* 	  randx = pos % screen->w; */
-	      /* 	  randy = pos / screen->w; */
+		  /* 	  pos = (unsigned)total * (screen->w / 10) % (screen->h * screen->w - 1) * next_screen->format->BytesPerPixel; */
+		  /* 	  randx = pos % screen->w; */
+		  /* 	  randy = pos / screen->w; */
 	      
-	      /* 	  pos = randy * next_screen->pitch + randx * next_screen->format->BytesPerPixel; */
-	      /* fast pseudo random */
-	      /* pos += pos + (total % 10); */
+		  /* 	  pos = randy * next_screen->pitch + randx * next_screen->format->BytesPerPixel; */
+		  /* fast pseudo random */
+		  /* pos += pos + (total % 10); */
 	      
-	      /* 	  pos += increment; */
-	      /* 	  pos %= pos_limit; */
-	      /* 	  if (pos > pos_limit) pos = 0; */
+		  /* 	  pos += increment; */
+		  /* 	  pos %= pos_limit; */
+		  /* 	  if (pos > pos_limit) pos = 0; */
 	      
-	      pos = myrand(pos_limit);
+		  pos = myrand(pos_limit);
 	      
-	      /* 	  if (pos > pos_limit) pos = 0; */
-	      array_index = pos * next_screen->format->BytesPerPixel;
-	      {
-		char *address1;
-		char *address2;
+		  /* 	  if (pos > pos_limit) pos = 0; */
+		  array_index = pos * next_screen->format->BytesPerPixel;
+		  {
+		    char *address1;
+		    char *address2;
 		
-		address1 = &(((char*)screen->pixels)[array_index]);
-		address2 = &(((char*)next_screen->pixels)[array_index]);
-		memcpy(address1, address2,
-		       next_screen->format->BytesPerPixel);
+		    address1 = &(((char*)screen->pixels)[array_index]);
+		    address2 = &(((char*)next_screen->pixels)[array_index]);
+		    memcpy(address1, address2,
+			   next_screen->format->BytesPerPixel);
 		
-		address1 += increment;
-		address2 += increment;
-		memcpy(address1, address2,
-		       next_screen->format->BytesPerPixel);
+		    address1 += increment;
+		    address2 += increment;
+		    memcpy(address1, address2,
+			   next_screen->format->BytesPerPixel);
 		
-		address1 += increment;
-		address2 += increment;
-		memcpy(address1, address2,
-		       next_screen->format->BytesPerPixel);
+		    address1 += increment;
+		    address2 += increment;
+		    memcpy(address1, address2,
+			   next_screen->format->BytesPerPixel);
 		
-		address1 += increment;
-		address2 += increment;
-		memcpy(address1, address2,
-		       next_screen->format->BytesPerPixel);
-	      }
-	      /* 	  SDL_Rect src_dst = {randx, randy, 1, 1}; */
-	      /* 	  SDL_BlitSurface(next_screen, &src_dst, screen, &src_dst); */
-	      total++;
+		    address1 += increment;
+		    address2 += increment;
+		    memcpy(address1, address2,
+			   next_screen->format->BytesPerPixel);
+		  }
+		  /* 	  SDL_Rect src_dst = {randx, randy, 1, 1}; */
+		  /* 	  SDL_BlitSurface(next_screen, &src_dst, screen, &src_dst); */
+		  total++;
 	      
-	      /* Byte by byte, color per color */
-	      /* 	  { */
-	      /* 	    int pos = round(1.0 * (next_screen->pitch * next_screen->h - 1) * rand() / RAND_MAX); */
-	      /* 	    ((char*)screen->pixels)[pos] = ((char*)next_screen->pixels)[pos]; */
-	      /* 	  } */
+		  /* Byte by byte, color per color */
+		  /* 	  { */
+		  /* 	    int pos = round(1.0 * (next_screen->pitch * next_screen->h - 1) * rand() / RAND_MAX); */
+		  /* 	    ((char*)screen->pixels)[pos] = ((char*)next_screen->pixels)[pos]; */
+		  /* 	  } */
 	      
-	      /* 2x2 pixels each time */
-	      /* randx = (int) round(1.0 * (next_screen->w-1-1) * rand() / RAND_MAX); */
-	      /* randy = (int) round(1.0 * (next_screen->h-1-1) * rand() / RAND_MAX); */
+		  /* 2x2 pixels each time */
+		  /* randx = (int) round(1.0 * (next_screen->w-1-1) * rand() / RAND_MAX); */
+		  /* randy = (int) round(1.0 * (next_screen->h-1-1) * rand() / RAND_MAX); */
 	      
-	      /* 2x2 pixels each time, starting point every 2 pixels (finishes faster but uglier) */
-	      /* 	  randx = (int) round(1.0 * (next_screen->w/2-1) * rand() / RAND_MAX); */
-	      /* 	  randy = (int) round(1.0 * (next_screen->h/2-1) * rand() / RAND_MAX); */
-	      /* 	  randx *= 2; */
-	      /* 	  randy *= 2; */
+		  /* 2x2 pixels each time, starting point every 2 pixels (finishes faster but uglier) */
+		  /* 	  randx = (int) round(1.0 * (next_screen->w/2-1) * rand() / RAND_MAX); */
+		  /* 	  randy = (int) round(1.0 * (next_screen->h/2-1) * rand() / RAND_MAX); */
+		  /* 	  randx *= 2; */
+		  /* 	  randy *= 2; */
 	      
-	      /* 	  get_pixel(next_screen, randx, randy, &r, &g, &b); */
-	      /* 	  set_pixel(screen, randx, randy, r, g, b); */
+		  /* 	  get_pixel(next_screen, randx, randy, &r, &g, &b); */
+		  /* 	  set_pixel(screen, randx, randy, r, g, b); */
 	      
-	      /* 	  get_pixel(next_screen, randx+1, randy, &r, &g, &b); */
-	      /* 	  set_pixel(screen, randx+1, randy, r, g, b); */
+		  /* 	  get_pixel(next_screen, randx+1, randy, &r, &g, &b); */
+		  /* 	  set_pixel(screen, randx+1, randy, r, g, b); */
 	      
-	      /* 	  get_pixel(next_screen, randx, randy+1, &r, &g, &b); */
-	      /* 	  set_pixel(screen, randx, randy+1, r, g, b); */
+		  /* 	  get_pixel(next_screen, randx, randy+1, &r, &g, &b); */
+		  /* 	  set_pixel(screen, randx, randy+1, r, g, b); */
 	      
-	      /* 	  get_pixel(next_screen, randx+1, randy+1, &r, &g, &b); */
-	      /* 	  set_pixel(screen, randx+1, randy+1, r, g, b); */
+		  /* 	  get_pixel(next_screen, randx+1, randy+1, &r, &g, &b); */
+		  /* 	  set_pixel(screen, randx+1, randy+1, r, g, b); */
 	      
-	      /*  	  randx = (int) round(1.0 * (next_screen->w/2-1) * rand() / RAND_MAX); */
-	      /* 	  randy = (int) round(1.0 * (next_screen->h/2-1) * rand() / RAND_MAX); */
-	      /* 	  randx *= 2; */
-	      /* 	  randy *= 2; */
-	      /* 	  { */
-	      /* 	    SDL_Rect src_dst = {randx, randy, 2, 2}; */
-	      /* 	    SDL_BlitSurface(next_screen, &src_dst, screen, &src_dst); */
-	      /* 	  } */
+		  /*  	  randx = (int) round(1.0 * (next_screen->w/2-1) * rand() / RAND_MAX); */
+		  /* 	  randy = (int) round(1.0 * (next_screen->h/2-1) * rand() / RAND_MAX); */
+		  /* 	  randx *= 2; */
+		  /* 	  randy *= 2; */
+		  /* 	  { */
+		  /* 	    SDL_Rect src_dst = {randx, randy, 2, 2}; */
+		  /* 	    SDL_BlitSurface(next_screen, &src_dst, screen, &src_dst); */
+		  /* 	  } */
 	      
-	      /*  	  randx = (int) round(1.0 * (next_screen->w/3-1) * rand() / RAND_MAX); */
-	      /* 	  randy = (int) round(1.0 * (next_screen->h/3-1) * rand() / RAND_MAX); */
-	      /* 	  randx *= 3; */
-	      /* 	  randy *= 3; */
-	      /* 	  { */
-	      /* 	    SDL_Rect src_dst = {randx, randy, 3, 3}; */
-	      /* 	    SDL_BlitSurface(next_screen, &src_dst, screen, &src_dst); */
-	      /* 	  } */
+		  /*  	  randx = (int) round(1.0 * (next_screen->w/3-1) * rand() / RAND_MAX); */
+		  /* 	  randy = (int) round(1.0 * (next_screen->h/3-1) * rand() / RAND_MAX); */
+		  /* 	  randx *= 3; */
+		  /* 	  randy *= 3; */
+		  /* 	  { */
+		  /* 	    SDL_Rect src_dst = {randx, randy, 3, 3}; */
+		  /* 	    SDL_BlitSurface(next_screen, &src_dst, screen, &src_dst); */
+		  /* 	  } */
 	      
-	    }
+		}
 	  
-	  if (SDL_GetTicks() - last_update > (1000 / 50))
+	      if (SDL_GetTicks() - last_update > (1000 / 50))
+		{
+		  SDL_Flip(screen);
+		  last_update = SDL_GetTicks();
+		}
+	    }
+	  /* Transition 2 - Alpha blend */
+	  else if (transition == 2)
 	    {
-	      if (SDL_MUSTLOCK(screen))
-		SDL_UnlockSurface(screen);
+	      /* Target: duration=1/4sec */
+	      Uint32 target = 250;
+	      Uint32 elapsed_time = SDL_GetTicks() - start;
+	      if (elapsed_time < target)
+		{
+		  SDL_SetAlpha(next_screen, SDL_SRCALPHA, IMIN(255, 255 * (elapsed_time / (double)target)));
+		  SDL_BlitSurface(previous_screen, NULL, screen, NULL);
+		  SDL_BlitSurface(next_screen, NULL, screen, NULL);
+		  SDL_Flip(screen);
+		  total++; // nb frames
+		  SDL_framerateDelay(&framerate_manager);
+		}
+	      else
+		{
+		  SDL_SetAlpha(next_screen, 0, 0);
+		  skip = 1;
+		}
+	    }
+	  /* Progressive zoom */
+	  else if (transition == 3)
+	    {
+	      Uint32 elapsed_time = SDL_GetTicks() - start;
+	      double factor = 1 + elapsed_time * 0.01 / 1000; /* 1sec = 10% bigger */
+	      double target_w = screen->w * factor;
+	      double factor_orig = target_w / zoombase->w;
+	      SDL_Surface *zoom = zoomSurface(zoombase, factor_orig, factor_orig, SMOOTHING_OFF);
+	      SDL_SetAlpha(zoom, 0, 0); /* Work around SDL_gfx 2.0.16 (reported) bug */
+
+	      /* SDL_Surface *zoom = zoomSurface(next_screen, factor, factor, SMOOTHING_OFF); */
+	      SDL_BlitSurface(zoom, NULL, screen, NULL);
+	      SDL_FreeSurface(zoom);
 	      SDL_Flip(screen);
-	      if (SDL_MUSTLOCK(screen))
-		SDL_LockSurface(screen);
-	      last_update = SDL_GetTicks();
+	      total++; // nb frames
+	      SDL_framerateDelay(&framerate_manager);
 	    }
 	}
-      if (SDL_MUSTLOCK(screen))
-	SDL_UnlockSurface(screen);
-      printf("Modified pixels: %d\n", total);
-      printf("Elapsed time: %d\n", SDL_GetTicks() - start);
+      {
+	Uint32 elapsed = SDL_GetTicks() - start;
+	printf("Frames: %d\n", total);
+	printf("Elapsed time: %d\n", elapsed);
+	printf("Framerate: %0.1f/s\n", total / (elapsed / 1000.0));
+      }
+      SDL_FreeSurface(zoombase);
+
       SDL_BlitSurface(next_screen, NULL, screen, NULL);
       SDL_Flip(screen);
 
+      /**
+       * Wait
+       */
       /* Preload next image if we're not at the last image already */
       if (i < (nb_images-1) && !quit)
-	  background_loading = SDL_CreateThread(load_image, (void*)(i+1));
+	background_loading = SDL_CreateThread(load_image, (void*)(i+1));
 
       SDL_Event event;
       SDL_TimerID timer_id = SDL_AddTimer(5000, StopPauseCallback, NULL);
@@ -483,6 +555,7 @@ int main(int argc, char *argv[])
     SDL_FreeSurface(image);
   if (resized != NULL)
     SDL_FreeSurface(resized);
+  printf("res=%d\n", screen->format->BitsPerPixel);
   SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_TIMER);
   //SDL_Quit();
   return 0;
